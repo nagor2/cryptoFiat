7d93b8f9c3bb2c424e18c02010dcde518bb855ae
d.nagornykh <nagor@academ.org>
2022-12-26 12:18:25 +0700

    some tests added

---
+++ test/cdpUpdateIncrease_test.js
@@ -0,0 +1,84 @@
+const { time } = require('@openzeppelin/test-helpers');
+
+var CDP = artifacts.require("./CDP.sol");
+var INTDAO = artifacts.require("./INTDAO.sol");
+var Oracle = artifacts.require("./Oracle.sol");
+var StableCoin = artifacts.require("./stableCoin.sol");
+
+const truffleAssert = require('truffle-assertions');
+
+contract('CDP Update', (accounts) => {
+    let dao;
+    let cdp;
+    let oracle;
+    let stableCoin;
+    let position;
+    let positionID;
+    let positionUpdate;
+
+    before('should setup the contracts instance', async () => {
+        dao = await INTDAO.deployed();
+        oracle = await Oracle.deployed(dao.address);
+        stableCoin = await StableCoin.deployed(dao.address);
+        cdp = await CDP.deployed(dao.address);
+
+        positionID = await cdp.openCDP(web3.utils.toWei('2000', 'ether'), {
+            from: accounts[2],
+            value: web3.utils.toWei('1', 'ether')
+        });
+
+        posId = 0;
+
+        expectedOwner = accounts[2];
+
+        position = await cdp.positions(posId);
+
+        console.log(expectedOwner);
+        console.log(position.owner);
+
+        positionUpdate = await cdp.updateCDP(posId, web3.utils.toWei('100', 'ether'), {from: accounts[2],value: web3.utils.toWei('1', 'ether')});
+    });
+
+    it("should emit PositionUpdated", async () => {
+        truffleAssert.eventEmitted(positionUpdate, 'PositionUpdated', async (ev) => {
+            it("should emit proper parameters", async () => {
+                assert.equal(ev.posID, 0, 'positionID is wrong');
+                assert.equal(ev.newStableCoinsAmount.toNumber(), web3.utils.toWei('2100', 'ether'), 'amount is wrong');
+            });
+        });
+    });
+
+    /*
+
+    it("should emit Mint", async () => {
+        truffleAssert.eventEmitted(positionUpdate, 'Mint', async (ev) => {
+            return ev.to === expectedOwner && ev.amount.toNumber() === web3.utils.toWei('100', 'ether');
+        });
+    });
+    */
+
+    it("should increase ethAmount locked", async () => {
+        position = await cdp.positions(posId);
+        assert.equal(position.ethAmountLocked, web3.utils.toWei('2','ether'), "ethAmountLocked should be 2 ethers");
+    });
+
+    it("should put 1 ether on contract's balance", async () => {
+        const contractBalance = await web3.eth.getBalance(cdp.address);
+        assert.equal(contractBalance, web3.utils.toWei('2','ether'), "contract's balance should be 2 ethers");
+    });
+
+    it("should increase owner balance", async () => {
+        const balance = await stableCoin.balanceOf(position.owner);
+        assert.equal(balance, web3.utils.toWei('2100', 'ether'), "owner's balance should be 2100 stableCoin");
+    });
+
+    it("should mint coins if possible", async () => {
+        //const contractBalance = await web3.eth.getBalance(cdp.address);
+        //assert.equal(contractBalance, web3.utils.toWei('1','ether'), "contract's balance should be 1 ether");
+    });
+
+    it("burn coins if needed", async () => {
+        //const contractBalance = await web3.eth.getBalance(cdp.address);
+        //assert.equal(contractBalance, web3.utils.toWei('1','ether'), "contract's balance should be 1 ether");
+    });
+});
--- test/cdp_test.js
+++ test/cdp_test.js
@@ -15,30 +15,33 @@ contract('CDP', (accounts) => {
     let oracle;
     let expectedOwner;
     let stableCoin;
+    let posNumber;
+    let positionID;
 
     before('should setup the contracts instance', async () => {
-        rule = await Rule.deployed();
-        oracle = await Oracle.deployed();
-        stableCoin = await StableCoin.deployed();
-        dao = await INTDAO.deployed(rule.address, oracle.address, stableCoin.address);
+        dao = await INTDAO.deployed();
+        rule = await Rule.deployed(dao.address);
+        oracle = await Oracle.deployed(dao.address);
+        stableCoin = await StableCoin.deployed(dao.address);
         cdp = await CDP.deployed(dao.address);
-        await stableCoin.setCPDAddressOnce(cdp.address);
-    });
 
-    describe("creating a CDP and retrieving position fields", async () => {
-        let posNumber;
-        let positionID;
+        posNumber = await cdp.numPositions();
 
-        before("openCDP using accounts[1] and paying 1 ether", async () => {
-            posNumber = await cdp.numPositions();
-            positionID = await cdp.openCDP(web3.utils.toWei('8000','ether') , { from: accounts[1], value:  web3.utils.toWei('1','ether') });
-            expectedOwner = accounts[1];
+        positionID = await cdp.openCDP(web3.utils.toWei('8000', 'ether'), {
+            from: accounts[1],
+            value: web3.utils.toWei('1', 'ether')
         });
 
+        //console.log (positionID + " - positionID"); posID vs posNumber! posID - tx. Should make a tricky convert to int
 
-        it("should create a valid Position", async () => {
-            const position = await cdp.positions(0);
+        expectedOwner = accounts[1];
+    });
+
+    it("should create a valid Position", async () => {
+            //console.log(stableCoin.address + " " +dao.addresses['stableCoin']);
+            const position = await cdp.positions(posNumber);
             assert.equal(position.owner, expectedOwner, "position.owner should be "+expectedOwner);
+            //console.log ("position.owner: "+position.owner);
             assert.equal(position.ethAmountLocked, web3.utils.toWei('1','ether'), "ethAmountLocked should be 1 ether");
             const blockNum = await web3.eth.getBlockNumber();
             const block = await web3.eth.getBlock(blockNum);
@@ -48,55 +51,71 @@ contract('CDP', (accounts) => {
 
             const rate = await dao.params('interestRate');
 
-            //const balance = await stableCoin.balanceOf(accounts[1]);
-            //console.log ('balance: '+ balance);
-
             expect(rate).to.eql(position.feeRate,"fee rate should be set to dao.params value"); //compare 2 BN
             assert.equal(position.stableCoins_minted, 2170 * (10**18), "should mint 2170*10^18 stableCoins");
-            //assert.equal(balance, 2170 * (10**18), "should but 2170*10^18 stableCoins on owner's account");
-        });
+    });
 
+    it("should mint max 2170 coins per 1 ether", async () => {
+        const coins = await cdp.getMaxStableCoinsToMint(web3.utils.toWei('3000', 'ether'), web3.utils.toWei('1', 'ether'));
+        assert.equal(coins, 2170 * (10**18), "should mint max 2170 coins per 1 ether");
+    });
 
-        it("time rewind", async () => {
+    it("should mint coins", async () => {
+        const position = await cdp.positions(posNumber);
+        const owner = await position.owner;
+        const coinsMinted = await position.stableCoins_minted;
+        const ballance = await stableCoin.balanceOf(owner);
+        assert.equal (coinsMinted.toString(), ballance.toString(), "notEqual mint coins");
+
+        //assert.equal(coins, 2170 * (10**18), "should mint max 2170 coins per 1 ether");
+    });
+
+    it("should mint desirable coins amount per 1 ether", async () => {
+        const coins = await cdp.getMaxStableCoinsToMint(8, web3.utils.toWei('1', 'ether'));
+        assert.equal(coins, 8, "should mint desirable coins amount per 1 ether");
+    });
+
+    it("time rewind", async () => {
             await time.increase(31536000);//1 year in seconds. It may sometimes fail
             const fee = await cdp.generatedFee(0);
+            //console.log (fee.toString());
+            assert.isTrue((fee >= 1953*10**17 && fee <= 1954*10**17), "should increase generated fee. It may sometimes fail due to time rewind (not precise)");
+    });
 
-            console.log (fee.toString());
-            assert.equal(fee, 1953*10**17, "should increase generated fee. It may sometimes fail");
-        });
-
-        it("should return valid position ID", async () => {
+    it("should return valid position ID", async () => {
             const numPos = await cdp.numPositions.call();
             truffleAssert.eventEmitted(positionID, 'PositionOpened', (ev) => {
                 return ev.owner === accounts[1] && ev.posId.toNumber() === numPos-1;
             });
-        });
+    });
 
-        it("should increase numPositions", async () => {
+    it("should increase numPositions", async () => {
             const numPos = await cdp.numPositions();
             assert.equal(numPos, parseInt(posNumber)+1, "number of positions should increase while opening");
-        });
+    });
 
-        it("should put 1 ether on contract's balance", async () => {
+    it("should put 1 ether on contract's balance", async () => {
+            const contractBalance = await web3.eth.getBalance(cdp.address);
+            assert.equal(contractBalance, web3.utils.toWei('1','ether'), "contract's balance should be 1 ether");
+    });
+
+
+
+    describe("update CDP", async () => {
+        it("should open CDP then approve some coins and then decrease amount", async () => {
             const contractBalance = await web3.eth.getBalance(cdp.address);
             assert.equal(contractBalance, web3.utils.toWei('1','ether'), "contract's balance should be 1 ether");
         });
-
     });
 
-    describe("max stableCoinsToMint", async () => {
-        it("should mint max 2170 coins per 1 ether", async () => {
-            const coins = await cdp.getMaxStableCoinsToMint(web3.utils.toWei('3000', 'ether'), web3.utils.toWei('1', 'ether'));
-            assert.equal(coins, 2170 * (10**18), "should mint max 2170 coins per 1 ether");
-        });
+});
 
-        it("should mint desirable coins amount per 1 ether", async () => {
-            const coins = await cdp.getMaxStableCoinsToMint(8, web3.utils.toWei('1', 'ether'));
-            assert.equal(coins, 8, "should mint desirable coins amount per 1 ether");
-        });
-    });
 
-    describe("update CDP", async () => {
+
+
+
+
+        /*
         let txOpen;
         let txUpdate;
               before("update CDP should open CDP then approve some coins and then decrease amount", async () => {
@@ -115,36 +134,4 @@ contract('CDP', (accounts) => {
               });
 
               //Burned(address(this), amount);
-        //PositionUpdated(p.owner, newStableCoinsAmount);
-        //
-/*
-              it("should create a valid Position", async () => {
-                  const position = await cdp.positions(0);
-                  assert.equal(position.owner, expectedOwner, "position.owner should be "+expectedOwner);
-                  assert.equal(position.ethAmountLocked, web3.utils.toWei('1','ether'), "ethAmountLocked should be 1 ether");
-                  const blockNum = await web3.eth.getBlockNumber();
-                  const block = await web3.eth.getBlock(blockNum);
-                  const now = block.timestamp;
-                  assert.equal(position.timeOpened, now, "time of the position should be set to now");
-                  assert.equal(position.feeGenerated, 0, "fee generated should be set to 0");
-
-                  const rate = await dao.params('interestRate');
-                  expect(rate).to.eql(position.feeRate,"fee rate should be set to dao.params value"); //compare 2 BN
-                  assert.equal(position.stableCoins_minted, 2170 * (10**18), "should mint 2170*10^18 stableCoins");
-              });
-
-              it("time rewind", async () => {
-                  await time.increase(31535999);//1 year in seconds. It may sometimes fail
-                  const fee = await cdp.generatedFee(0);
-
-                  console.log (fee.toString());
-                  assert.equal(fee, 1953*10**17, "should increase generated fee. It may sometimes fail");
-              });*/
-
-    });
-
-
-
-});
-
-
+        //PositionUpdated(p.owner, newStableCoinsAmount);*/
